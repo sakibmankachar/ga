c48796a9a1ac6f2a3027a6637b04d93140f69623
Glitch (cord-cultured-houseboat) <none>
2021-03-06 09:15:56 +0000

    🌏👍 Checkpoint ./commands/warn.js:27141663/1541 ./commands/unwarn.js:27141663/786

--- commands/unwarn.js
+++ commands/unwarn.js
@@ -1,11 +1,11 @@
 module.exports = {
-name: "unwarn",
-code: `
-$setServerVar[warns;$sub[$mentioned[1];$getServerVar[warns;$mentioned[1]];1]]
+  name: "unwarn",
+  code: `
+$setUserVar[warns;$sub[$mentioned[1];$getUserVar[warns;$mentioned[1]];1]]
 $title[Removed Warn]
 $description[This User Warn has been removed!]
 $footer[This User now has $getServarVar[warn] warns]
 $onlyIf[$getServerVar[warn]!==0;You can unwarn This user. He/She has no warns.]
 $onlyPerms[manageserver;admin;❌You can't Use this command]
 `
-}
+};
