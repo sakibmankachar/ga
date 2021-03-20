0434c901eb0266418dfa01633965c3fdbb53a40c
Glitch (a-sak-bot) <none>
2021-03-20 15:30:48 +0000

    🌁💷 Checkpoint ./commands/JALLOGS guide.js:27979027/984 ./commands/guide.js:27979027/6

--- commands/guide.js
+++ commands/guide.js
@@ -3,7 +3,7 @@ module.exports = {
   code: `$title[GUIDE]
 $description[Wanna to learn how to create and code a discord bot on Android with Bot designer for discord?
 Then download this file and learn
-Download link [Click Here](https://www.dropbox.com/s/caniq99sxmen9rr/Guide%20For%20HTCACADBOAWBDFD.doc?dl=0) or [Click Here](https://cdn.discordapp.com/attachments/798154805699805234/800274401320894474/Guide_For_HTCACADBOAWBDFD.doc)]
+Download link [Click Here\\](https://www.dropbox.com/s/caniq99sxmen9rr/Guide%20For%20HTCACADBOAWBDFD.doc?dl=0) or [Click Here\\](https://cdn.discordapp.com/attachments/798154805699805234/800274401320894474/Guide_For_HTCACADBOAWBDFD.doc)]
 $color[#ff2052]
 $footer[THANKS FOR READING!]`
 }
